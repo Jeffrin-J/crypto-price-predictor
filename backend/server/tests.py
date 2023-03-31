d9c05ca0f2e9ab1aba20c2bb4f9c59c52f685fd4
Jeffrin-J <jeffrinjacob02@gmail.com>
2023-03-31 20:20:04 +0530

    update

--- backend/server/tests.py
+++ backend/server/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
 # Create your tests here.
+#dot
+# df_month2.loc['2022-03-31'] = 30
+# df_month2.loc['2022-02-28'] = 26
+# df_month2.loc['2021-03-31'] = 35
+# df_month2.loc['2021-02-28'] = 30
+
+# df_month2.loc['2021-05-31'] = 58500
+# df_month2.loc['2021-11-30'] = 62000
+# df_month2.loc['2021-12-31'] = 58000
+
+#doge
+# df_month2.loc['2022-01-31'] = 0.11
+
+#lite
+# df_month2.loc['2018-01-31'] = 174.6
+# df_month2.loc['2018-02-28'] = 225
+# df_month2.loc['2018-03-31'] = 152.2
+# df_month2.loc['2018-04-30'] = 136
+# df_month2.loc['2018-05-31'] = 138
+# df_month2.loc['2018-06-30'] = 80
+# df_month2.loc['2018-07-31'] = 50
+# df_month2.loc['2018-08-31'] = 52
+# df_month2.loc['2018-12-31'] = 20
+
+#avax
+# df_month2.loc['2022-01-31'] = 100
+# df_month2.loc['2022-02-28'] = 78
+# df_month2.loc['2022-03-31'] = 76
+# df_month2.loc['2022-04-30'] = 70
+# df_month2.loc['2022-08-31'] = 19
